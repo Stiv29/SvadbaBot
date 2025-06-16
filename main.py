f25044b13a36147bf177fc3b6d387a5e89d01d54
Stiv29 <123989337+Stiv29@users.noreply.github.com>
2025-06-16 10:38:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
     app.run_polling()
 
 if __name__ == "__main__":
